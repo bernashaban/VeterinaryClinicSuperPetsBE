ed48c7d508b8c9e1f4088b7cde64c8dedc8ab1b0
Berna Shaban <berna.shaban@brainstarstech.com>
2023-04-03 23:24:01 +0300

    dto for owner added

---
+++ src/main/java/com/example/veterinaryclinicsuperpets/dto/owner/OwnerRequest.java
@@ -0,0 +1,17 @@
+package com.example.veterinaryclinicsuperpets.dto.owner;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OwnerRequest {
+  private String fullName;
+  private String address;
+  private String email;
+  private String phoneNum;
+  private String username;
+  private String password;
+}
