a66dd64c9f719a2ba3413da13a0fd7a1e1520492
Berna Shaban <berna.shaban@brainstarstech.com>
2023-04-03 22:42:11 +0300

    admin class added

---
+++ src/main/java/com/example/veterinaryclinicsuperpets/entity/Admin.java
@@ -0,0 +1,32 @@
+package com.example.veterinaryclinicsuperpets.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "admin")
+@Entity
+public class Admin {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+}
