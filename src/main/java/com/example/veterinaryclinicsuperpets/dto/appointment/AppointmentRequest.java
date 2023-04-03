931bc51149194e034bb13b76ac38c23c59c0b36e
Berna Shaban <berna.shaban@brainstarstech.com>
2023-04-03 23:13:54 +0300

    dto for appointments added

---
+++ src/main/java/com/example/veterinaryclinicsuperpets/dto/appointment/AppointmentRequest.java
@@ -0,0 +1,21 @@
+package com.example.veterinaryclinicsuperpets.dto.appointment;
+
+import com.example.veterinaryclinicsuperpets.entity.Owner;
+import com.example.veterinaryclinicsuperpets.entity.Pet;
+import com.example.veterinaryclinicsuperpets.entity.Veterinarian;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentRequest {
+  private Owner owner;
+  private Pet pet;
+  private Veterinarian veterinarian;
+  private LocalDateTime dateTime;
+  private String description;
+}
