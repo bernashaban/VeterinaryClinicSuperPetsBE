27ada4a3f38ec09d67e86a74f76542ebcd84caa5
Berna Shaban <berna.shaban@brainstarstech.com>
2023-04-04 23:07:20 +0300

    mapper classes implemented

---
+++ src/main/java/com/example/veterinaryclinicsuperpets/mapper/impl/AssistanceMapperImpl.java
@@ -0,0 +1,35 @@
+package com.example.veterinaryclinicsuperpets.mapper.impl;
+
+import com.example.veterinaryclinicsuperpets.dto.assistence.AssistanceRequest;
+import com.example.veterinaryclinicsuperpets.dto.assistence.AssistanceResponse;
+import com.example.veterinaryclinicsuperpets.entity.Assistance;
+import com.example.veterinaryclinicsuperpets.mapper.AssistanceMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AssistanceMapperImpl implements AssistanceMapper {
+  @Override
+  public AssistanceResponse entityToResponse(Assistance entity) {
+    AssistanceResponse assistanceResponse = new AssistanceResponse();
+    assistanceResponse.setId(entity.getId());
+    assistanceResponse.setName(entity.getName());
+    assistanceResponse.setPrice(entity.getPrice());
+    assistanceResponse.setServiceType(entity.getServiceType());
+    return assistanceResponse;
+  }
+
+  @Override
+  public Assistance requestToEntity(AssistanceRequest entityRequest) {
+    Assistance assistance = new Assistance();
+    assistance.setName(entityRequest.getName());
+    assistance.setPrice(entityRequest.getPrice());
+    assistance.setServiceType(entityRequest.getServiceType());
+    return assistance;
+  }
+
+  @Override
+  public List<AssistanceResponse> listOfEntitiesToListOfResponses(List<Assistance> entities) {
+    return entities.stream().map(this::entityToResponse).collect(Collectors.toList());
+  }
+}
