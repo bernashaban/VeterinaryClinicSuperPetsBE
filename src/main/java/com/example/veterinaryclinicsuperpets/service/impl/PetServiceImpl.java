323f248f808d04e4f86b1fbbe69f68ca2f5ea825
Berna Shaban <berna.shaban@brainstarstech.com>
2023-04-15 01:04:37 +0300

    get by id method added

--- src/main/java/com/example/veterinaryclinicsuperpets/service/impl/PetServiceImpl.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/service/impl/PetServiceImpl.java
@@ -2,6 +2,10 @@ package com.example.veterinaryclinicsuperpets.service.impl;
 
 import com.example.veterinaryclinicsuperpets.dto.pet.PetRequest;
 import com.example.veterinaryclinicsuperpets.dto.pet.PetResponse;
+import com.example.veterinaryclinicsuperpets.entity.Appointment;
+import com.example.veterinaryclinicsuperpets.entity.Pet;
+import com.example.veterinaryclinicsuperpets.mapper.PetMapper;
+import com.example.veterinaryclinicsuperpets.repository.PetRepository;
 import com.example.veterinaryclinicsuperpets.service.PetService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,28 +15,32 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class PetServiceImpl implements PetService {
-    @Override
-    public PetResponse getById(Long id) {
-        return null;
-    }
+  private final PetRepository petRepository;
+  private final PetMapper petMapper;
 
-    @Override
-    public Long create(PetRequest request) {
-        return null;
-    }
+  @Override
+  public PetResponse getById(Long id) {
+    Pet pet = petRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    return petMapper.entityToResponse(pet);
+  }
 
-    @Override
-    public PetResponse delete(Long id) {
-        return null;
-    }
+  @Override
+  public Long create(PetRequest request) {
+    return null;
+  }
 
-    @Override
-    public PetResponse update(PetRequest request, Long id) {
-        return null;
-    }
+  @Override
+  public PetResponse delete(Long id) {
+    return null;
+  }
 
-    @Override
-    public List<PetResponse> getAll() {
-        return null;
-    }
+  @Override
+  public PetResponse update(PetRequest request, Long id) {
+    return null;
+  }
+
+  @Override
+  public List<PetResponse> getAll() {
+    return null;
+  }
 }
