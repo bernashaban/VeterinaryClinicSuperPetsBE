333adcbf9e561537099ac371df91bbf9e95a809e
Berna Shaban <bebozcan@gmail.com>
2023-04-15 11:12:36 +0300

    equals and hashCode methods implemented

--- src/main/java/com/example/veterinaryclinicsuperpets/entity/Pet.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/entity/Pet.java
@@ -14,6 +14,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -36,4 +38,17 @@ public class Pet {
   private PetType type;
 
   @ManyToOne private Owner owner;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Pet pet = (Pet) o;
+    return Objects.equals(id, pet.id) && Objects.equals(owner, pet.owner);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, owner);
+  }
 }
