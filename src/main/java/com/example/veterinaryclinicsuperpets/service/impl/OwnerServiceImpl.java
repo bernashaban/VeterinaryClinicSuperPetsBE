9a6c9fa1357b9bfc832daa994faa6f3cb4e4d9a9
Berna Shaban <bebozcan@gmail.com>
2023-04-15 12:54:52 +0300

    validations added

--- src/main/java/com/example/veterinaryclinicsuperpets/service/impl/OwnerServiceImpl.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/service/impl/OwnerServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.veterinaryclinicsuperpets.entity.Owner;
 import com.example.veterinaryclinicsuperpets.mapper.OwnerMapper;
 import com.example.veterinaryclinicsuperpets.repository.OwnerRepository;
 import com.example.veterinaryclinicsuperpets.service.OwnerService;
+import com.example.veterinaryclinicsuperpets.util.validations.Validator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +26,19 @@ public class OwnerServiceImpl implements OwnerService {
 
   @Override
   public Long create(OwnerRequest request) {
+    if (!Validator.validateEmail(request.getEmail())) {
+      throw new IllegalArgumentException("Email is not valid!");
+    }
+    if (Validator.validatePhoneNum(request.getPhoneNum())) {
+      throw new IllegalArgumentException("Phone number is not valid! Should start whit '+359'");
+    }
+    if (Validator.validateUsername(request.getUsername())) {
+      throw new IllegalArgumentException("Username is not valid!");
+    }
+    if (Validator.validatePassword(request.getPassword())) {
+      throw new IllegalArgumentException(
+          "Password is not valid! Minimum eight characters, at least one letter and one number.");
+    }
     Owner owner = ownerMapper.requestToEntity(request);
     return ownerRepository.save(owner).getId();
   }
@@ -46,15 +60,28 @@ public class OwnerServiceImpl implements OwnerService {
       owner.setAddress(request.getAddress());
     }
     if (!request.getEmail().equals(owner.getEmail())) {
+      if (!Validator.validateEmail(request.getEmail())) {
+        throw new IllegalArgumentException("Email is not valid!");
+      }
       owner.setEmail(request.getEmail());
     }
     if (!request.getUsername().equals(owner.getUsername())) {
+      if (Validator.validateUsername(request.getUsername())) {
+        throw new IllegalArgumentException("Username is not valid!");
+      }
       owner.setUsername(request.getUsername());
     }
     if (!request.getPassword().equals(owner.getPassword())) {
+      if (Validator.validatePassword(request.getPassword())) {
+        throw new IllegalArgumentException(
+            "Password is not valid! Minimum eight characters, at least one letter and one number.");
+      }
       owner.setPassword(request.getPassword());
     }
     if (!request.getPhoneNum().equals(owner.getPhoneNum())) {
+      if (Validator.validatePhoneNum(request.getPhoneNum())) {
+        throw new IllegalArgumentException("Phone number is not valid! Should start whit '+359'");
+      }
       owner.setPhoneNum(request.getPhoneNum());
     }
     return ownerMapper.entityToResponse(owner);
