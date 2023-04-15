eec33b5e7747937fcc6ac3d77293b4b1b8c21e69
Berna Shaban <bebozcan@gmail.com>
2023-04-15 11:32:51 +0300

    pet controller implemented

---
+++ src/main/java/com/example/veterinaryclinicsuperpets/controller/PetController.java
@@ -0,0 +1,48 @@
+package com.example.veterinaryclinicsuperpets.controller;
+
+import com.example.veterinaryclinicsuperpets.dto.pet.PetRequest;
+import com.example.veterinaryclinicsuperpets.dto.pet.PetResponse;
+import com.example.veterinaryclinicsuperpets.service.PetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pet")
+@RequiredArgsConstructor
+public class PetController {
+  private final PetService petService;
+
+  @GetMapping("/{id}")
+  public PetResponse getPet(@PathVariable Long id) {
+    return petService.getById(id);
+  }
+
+  @GetMapping()
+  public List<PetResponse> getAllPets() {
+    return petService.getAll();
+  }
+
+  @DeleteMapping("/{id}")
+  public PetResponse deleteCourse(@PathVariable Long id) {
+    return petService.delete(id);
+  }
+
+  @PostMapping()
+  public Long postCourse(@RequestBody PetRequest request) {
+    return petService.create(request);
+  }
+
+  @PutMapping(value = "/{id}")
+  public PetResponse updateCourse(@RequestBody PetRequest request, @PathVariable Long id) {
+    return petService.update(request, id);
+  }
+}
