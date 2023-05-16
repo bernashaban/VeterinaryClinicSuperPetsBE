4b42fae3c9f76b6551cdce4219917aff43296d7f
Berna Shaban <bebozcan@gmail.com>
2023-05-17 00:24:01 +0300

    implemented JWT security

--- src/main/java/com/example/veterinaryclinicsuperpets/entity/Owner.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/entity/Owner.java
@@ -1,27 +1,34 @@
 package com.example.veterinaryclinicsuperpets.entity;
 
+import com.example.veterinaryclinicsuperpets.entity.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
+@Builder
 @Table(name = "owner")
 @Entity
-public class Owner {
+public class Owner implements UserDetails {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -44,6 +51,9 @@ public class Owner {
   @Column(nullable = false)
   private String password;
 
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -56,4 +66,30 @@ public class Owner {
   public int hashCode() {
     return Objects.hash(id, email, phoneNum, username);
   }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
 }
---
+++ src/main/java/com/example/veterinaryclinicsuperpets/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.veterinaryclinicsuperpets.entity.enums;
+
+public enum Role {
+    OWNER,
+    ADMIN
+}
