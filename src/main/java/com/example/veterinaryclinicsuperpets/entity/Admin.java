b00bc8dce755cbce5040003b6698754e7e10c04c
Berna Shaban <bebozcan@gmail.com>
2023-05-30 15:04:25 +0300

    added role to admin

--- src/main/java/com/example/veterinaryclinicsuperpets/entity/Admin.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/entity/Admin.java
@@ -1,7 +1,10 @@
 package com.example.veterinaryclinicsuperpets.entity;
 
+import com.example.veterinaryclinicsuperpets.entity.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,6 +14,12 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,7 +28,7 @@ import lombok.Setter;
 @Getter
 @Table(name = "admin")
 @Entity
-public class Admin {
+public class Admin implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -29,4 +38,32 @@ public class Admin {
 
   @Column(nullable = false)
   private String password;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
