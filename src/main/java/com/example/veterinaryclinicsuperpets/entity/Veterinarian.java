3e3204abee02a13644e33166afafb8ee88a3159f
Berna Shaban <bebozcan@gmail.com>
2023-05-30 15:06:03 +0300

    added role to veterinarian

--- src/main/java/com/example/veterinaryclinicsuperpets/entity/Veterinarian.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/entity/Veterinarian.java
@@ -1,7 +1,10 @@
 package com.example.veterinaryclinicsuperpets.entity;
 
+import com.example.veterinaryclinicsuperpets.entity.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,7 +14,13 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Data
@@ -21,7 +30,7 @@ import java.util.Objects;
 @Getter
 @Table(name = "veterinarian")
 @Entity
-public class Veterinarian {
+public class Veterinarian implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -50,6 +59,9 @@ public class Veterinarian {
   @Column(nullable = false)
   private String password;
 
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -61,5 +73,30 @@ public class Veterinarian {
   @Override
   public int hashCode() {
     return Objects.hash(id, email, username);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 }
