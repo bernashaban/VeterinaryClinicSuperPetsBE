fb030ba2eb9abd5f044dd2be75da513b340aad2c
Berna Shaban <bebozcan@gmail.com>
2023-05-30 15:05:34 +0300

    added validations for duplicate values

--- src/main/java/com/example/veterinaryclinicsuperpets/util/validations/Validator.java
+++ src/main/java/com/example/veterinaryclinicsuperpets/util/validations/Validator.java
@@ -1,9 +1,17 @@
 package com.example.veterinaryclinicsuperpets.util.validations;
 
+import com.example.veterinaryclinicsuperpets.repository.AdminRepository;
+import com.example.veterinaryclinicsuperpets.repository.OwnerRepository;
+import com.example.veterinaryclinicsuperpets.repository.VeterinarianRepository;
+import jakarta.xml.bind.ValidationException;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
+  private final VeterinarianRepository vetRepository;
+  private final OwnerRepository ownerRepository;
+  private final AdminRepository adminRepository;
 
   private static final String emailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
   private static final String phoneNumRegex = "^(\\+)?(359|0)8[789]\\d{1}(|-| )\\d{3}(|-| )\\d{3}$";
@@ -15,22 +23,44 @@ public class Validator {
   private static final Pattern usernamePattern = Pattern.compile(userNameRegex);
   private static final Pattern passwordPattern = Pattern.compile(passwordRegex);
 
-  public static boolean validateEmail(String email) {
+  public Validator(VeterinarianRepository vetRepository, OwnerRepository ownerRepository, AdminRepository adminRepository) {
+    this.vetRepository = vetRepository;
+    this.ownerRepository = ownerRepository;
+    this.adminRepository = adminRepository;
+  }
+
+  public boolean validateEmail(String email) throws ValidationException {
+    boolean isDuplicateVet = vetRepository.findByEmail(email)!=null;
+    boolean isDuplicateOwner = ownerRepository.findByEmail(email)!=null;
+    if(!isDuplicateVet  || !isDuplicateOwner){
+      throw new ValidationException("This email is already in use!");
+    }
     Matcher emailMatcher = emailPattern.matcher(email);
     return emailMatcher.matches();
   }
 
-  public static boolean validatePhoneNum(String phoneNum) {
+  public boolean validatePhoneNum(String phoneNum) throws ValidationException {
+    boolean isDuplicateVet = vetRepository.findByPhoneNum(phoneNum)!=null;
+    boolean isDuplicateOwner = ownerRepository.findByPhoneNum(phoneNum)!=null;
+    if(!isDuplicateVet  || !isDuplicateOwner){
+      throw new ValidationException("This phone number is already in use!");
+    }
     Matcher phoneNumMatcher = phoneNumPattern.matcher(phoneNum);
     return phoneNumMatcher.matches();
   }
 
-  public static boolean validateUsername(String username) {
+  public boolean validateUsername(String username) throws ValidationException {
+    boolean isDuplicateVet = vetRepository.findByUsername(username)!=null;
+    boolean isDuplicateOwner = ownerRepository.findByUsername(username).isPresent();
+    boolean isDuplicateAdmin = adminRepository.findByUsername(username)!=null;
+    if(!isDuplicateVet  || !isDuplicateOwner || !isDuplicateAdmin ){
+      throw new ValidationException("This username is already in use!");
+    }
     Matcher phoneNumMatcher = usernamePattern.matcher(username);
     return phoneNumMatcher.matches();
   }
 
-  public static boolean validatePassword(String password) {
+  public boolean validatePassword(String password) {
     // Minimum eight characters, at least one letter and one number
     Matcher passwordMatcher = passwordPattern.matcher(password);
     return passwordMatcher.matches();
